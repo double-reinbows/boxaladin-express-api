0126124ca5872aa0d47939f4035411a51c1c9226
irianto <irianto223@gmail.com>
2018-01-11 11:43:02 +0700

    tambahkan helper buat validasi jika phone number duplikat dan kalau sudah dipakai & terverifikasi oleh user lain

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const ctrl = require('../controller/indexCtrl')
 const phoneCtrl = require('../controller/otpCtrl')
+const phoneHelper = require('../helpers/phone')
 
 router.get('/', ctrl.getAll)
 router.get('/emailVerification', ctrl.verifyEmail)
@@ -15,6 +16,6 @@ router.post('/signin', ctrl.signin)
  * Endpoint post user data/info ketika signup
  */
 router.post('/signup', ctrl.signup)
-router.post('/phonenumber', phoneCtrl.postPhoneNumber)
+router.post('/phonenumber', phoneHelper.checkDuplicate, phoneHelper.checkAlready, phoneCtrl.postPhoneNumber)
 
 module.exports = router;
