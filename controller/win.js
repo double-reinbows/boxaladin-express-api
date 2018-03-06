dcbbb37ece86954f6f4f35b8dc51d83f56f332ab
irianto <irianto223@gmail.com>
2018-03-06 11:50:21 +0700

    tambah kondisi pemenang di create win

--- controller/win.js
+++ controller/win.js
@@ -32,7 +32,7 @@ module.exports = {
   create: (req, res) => {
     const decoded = jwt.verify(req.headers.token, process.env.JWT_SECRET)
 
-    const reward = req.body.star == 3 ? 'active1' : ( req.body.star == 2 ? 'active2' : 'active3' )
+    const reward = req.body.star == 5 ? 'active1' : ( req.body.star == 4 ? 'active2' : ( req.body.star == 3 ? 'active3' : ( req.body.star == 2 ? 'active4' : ( req.body.star == 1 ? 'active5' : '' ) ) ) )
     
     Model.reward.findOne({
       where: { [reward]: true }
@@ -43,27 +43,33 @@ module.exports = {
         return res.send({ errmsg: 'tidak ada hadiah buat jumlah star ini' })
       }
 
-      req.body.userId = decoded.id
-      req.body.rewardId = result.id
-      req.body.status = 'PENDING'
-      req.body.star = parseInt(req.body.star)
-
-      Model.win.create(req.body)
-      .then(result => {
-
-        Model.win.findOne({
-          where: {
-            id: result.id
-          },
-          include: [
-            { all: true }
-          ]
-        })
-        .then(result => res.send(result))
-        .catch(err => res.send(err))
-
+      return res.send({
+        msg: 'dapat free keys',
+        data: req.body,
+        data2: decoded
       })
-      .catch(err => res.send(err))
+
+      // req.body.userId = decoded.id
+      // req.body.rewardId = result.id
+      // req.body.status = 'PENDING'
+      // req.body.star = parseInt(req.body.star)
+
+      // Model.win.create(req.body)
+      // .then(result => {
+
+      //   Model.win.findOne({
+      //     where: {
+      //       id: result.id
+      //     },
+      //     include: [
+      //       { all: true }
+      //     ]
+      //   })
+      //   .then(result => res.send(result))
+      //   .catch(err => res.send(err))
+
+      // })
+      // .catch(err => res.send(err))
 
     })
     .catch(err => res.send(err))
