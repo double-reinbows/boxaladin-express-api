ba3195911c49906633e02030d764515279f7f574
Andrew <sbstn.andrew56@gmail.com>
2018-03-19 13:50:33 +0700

    change password allownul=true

--- migrations/20171220043401-create-boxaladin.js
+++ migrations/20171220043401-create-boxaladin.js
@@ -27,7 +27,7 @@ module.exports = {
       },
       password: {
         type: Sequelize.STRING,
-        allowNull: false
+        allowNull: true
       },
       email: {
         type: Sequelize.STRING,
