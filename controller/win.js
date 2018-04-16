e303d10086da686b049158c6d24c1a16268ac8f9
tezaharsony <teza.harsony230394@gmail.com>
2018-04-16 11:35:51 +0700

    change ref id for pulsa

--- controller/win.js
+++ controller/win.js
@@ -118,7 +118,7 @@ module.exports = {
     })
     .then(transactionResult => {
 
-      console.log('--- CREATE TRANSACTION RESULT --- :', transactionResult.dataValues)
+      // console.log('--- CREATE TRANSACTION RESULT --- :', transactionResult.dataValues)
 
       Model.product.findOne({
         where: {
